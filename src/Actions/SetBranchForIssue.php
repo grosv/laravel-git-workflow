065ab4ac4274a51e2b8b4c410a2c419c58f44a15
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2020-03-24 22:54:28 -0500

    Simplify

--- src/Actions/SetBranchForIssue.php
+++ src/Actions/SetBranchForIssue.php
@@ -11,48 +11,29 @@ use Symfony\Component\Process\Process;
 class SetBranchForIssue
 {
 
-    public function execute(string $issue): void
+    public array $branches;
+
+    public function __construct()
     {
-        $process = new Process(['git', 'branch', '-a']);
-        $process->run();
-        $branches = (new ParseGitBranches())->execute($process->getOutput());
+        $this->branches = [];
+    }
 
-        if (!in_array($issue, $branches)) {
-            $process = new Process(['git', 'checkout', '-b', $issue]);
-            $process->run();
-        }
-        else {
-            $process = new Process(['git', 'checkout', $issue]);
-            $process->run();
+    public function execute(string $issue, GitCommand $git, ParseGitBranches $branches): void
+    {
 
-            $process = new Process(['git', 'add', '.']);
-            $process->run();
+        $this->branches = $branches->execute($git->execute('git branch -a')->getOutput());
 
-            $process = new Process(['git', 'commit', '-m', 'WIP']);
-            $process->run();
+        if (in_array($issue, $this->branches)) {
+            $git->execute('git checkout ' . $issue);
+            $git->execute('git fetch');
+            $git->execute('git rebase origin/master');
+
+            return;
         }
 
-        if (!in_array('remotes/origin/' . $issue, $branches)) {
-            touch(base_path($issue));
-            File::append(base_path($issue), date('r') . ' Started Pull Request For ' . $issue);
-            $process = new Process(['git', 'add', '.']);
-            $process->run();
-
-            $process = new Process(['git', 'commit', '-m', 'WIP']);
-            $process->run();
-
-            $process = new Process(['git', 'push', '-u', 'origin', $issue]);
-            $process->run();
-
-            $process = new Process(['gh', 'pr', 'create', '-t', Str::title(str_replace('_', ' ', $issue)), '-b', 'WIP', '-d']);
-            $process->run();
-        }
-
-        $process = new Process(['git', 'fetch']);
-        $process->run();
-
-        $process = new Process(['git', 'rebase', 'origin/master']);
-        $process->run();
-
+        $git->execute('git checkout -b ' . $issue);
+        $git->execute('git commit --allow-empty -m "WIP"');
+        $git->execute('git push -u origin ' . $issue);
+        $git->execute('gh pr create -t ' . Str::title(str_replace('_', ' ', $issue)) . ' -b "WIP" -d');
     }
 }
