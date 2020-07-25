1afcb4b52602a31025eb193eae8934f7b897ab8e
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2020-07-25 17:13:12 +0000

    Apply fixes from StyleCI

--- src/Actions/ParseGitHubIssues.php
+++ src/Actions/ParseGitHubIssues.php
@@ -18,10 +18,11 @@ class ParseGitHubIssues
         foreach (explode("\n", $string) as $issue) {
             if (count(explode("\t", $issue)) > 1) {
                 $this->issues[explode("\t", $issue)[0]] = Str::snake(
-                    preg_replace( '/[^a-z0-9 ]/i', '', explode("\t", $issue)[0].' '.explode("\t", $issue)[1])
+                    preg_replace('/[^a-z0-9 ]/i', '', explode("\t", $issue)[0].' '.explode("\t", $issue)[1])
                 );
             }
         }
+
         return $this->issues;
     }
 }
